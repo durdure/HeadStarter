ae0193d7ab2209097d85b449bebb8a140db227ae
durdure <duresaeshetu2001@gmail.com>
2024-07-24 03:00:45 +1200

    headerstarter

--- personal_website/script.js
+++ personal_website/script.js
@@ -13,3 +13,31 @@ words.forEach((word) => {
 let currentTextIndex = 0;
 let maxTextIndex = words.length - 1;
 words[currentTextIndex].style.opacity = 1;
+
+let changeText = () => {
+    let currentWord = words[currentTextIndex];
+    let nextTextIndex = currentTextIndex === maxTextIndex ? 0 : currentTextIndex + 1;
+    let nextWord = words[nextTextIndex];
+
+    Array.from(currentWord.children).forEach((letter, i) => {
+        setTimeout(() => {
+            letter.className = 'letter out';
+        }, i * 80);
+    });
+
+    nextWord.style.opacity = 1;
+    Array.from(nextWord.children).forEach((letter, i) => {
+        letter.className = 'letter behind';
+        setTimeout(() => {
+            letter.className = 'letter in';
+        }, 340 + i * 80);
+    });
+
+    setTimeout(() => {
+        currentWord.style.opacity = 0;
+        currentTextIndex = nextTextIndex;
+    }, 340 + nextWord.children.length * 80);
+};
+
+changeText();
+setInterval(changeText, 4000);
