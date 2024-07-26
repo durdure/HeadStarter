85e492d7ad28e5ec508c9424a408c44a931cc2c8
durdure <duresaeshetu2001@gmail.com>
2024-07-26 09:21:39 -1200

    cool

--- personal_website/script.js
+++ personal_website/script.js
@@ -53,3 +53,26 @@ circles.forEach(elem=>{
     var points = "";
     var rotate = 0;
 })
+
+
+let menuLi = document.querySelectorAll('header ul li a');
+let section = document.querySelectorAll('section');
+
+
+
+function activeMenu() {
+    let len = section.length;
+    while(--len && window.scrollY + 100 < section[len].offsetTop) {}
+    menuLi.forEach(sec => sec.classList.remove('active'));
+    menuLi[len].classList.add('active');
+}
+
+
+activeMenu();
+window.addEventListener('scroll', activeMenu);
+
+
+const header = document.querySelector('header');
+window.addEventListener("scroll", function() {
+    header.classList.toggle('sticky', window.scrollY > 50);
+});
