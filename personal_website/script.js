937772360369f590a6c6b708c7b77128fcf68047
durdure <duresaeshetu2001@gmail.com>
2024-07-26 21:46:19 -1200

    cool

--- personal_website/script.js
+++ personal_website/script.js
@@ -76,3 +76,16 @@ const header = document.querySelector('header');
 window.addEventListener("scroll", function() {
     header.classList.toggle('sticky', window.scrollY > 50);
 });
+
+let menuIcon = document.querySelector('#menu-icon');
+let navlist  = document.querySelector('.navlist');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navlist.classList.toggle("open")
+}
+
+window.onscroll = () => {
+   menuIcon.classList.remove("bx-x");
+   menuIcon.classList.replace("bx-x", "bx-menu");
+}
