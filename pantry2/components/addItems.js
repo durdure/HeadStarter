2bb80d915d5d6a80af97c743bbb24232ec4289d7
Duresa Eshetu <duresaeshetu2001@gmail.com>
2024-08-01 10:30:21 +0300

    pantry

---
+++ pantry2/components/addItems.js
@@ -0,0 +1,10 @@
+import React , {useState}from 'react'
+import { TextField, Button } from '@mui/material';
+
+const addItems = () => {
+  return (
+    <div>addItems</div>
+  )
+}
+
+export default addItem
